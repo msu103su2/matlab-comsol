9992482b06147464e8cd06410003face78f84653
Purdy Lab <PURDYLAB@pitt.edu>
2019-10-03 16:15:18 -0400

    Bug fixed, now trying to optimize code for singlebeam

--- test.m
+++ test.m
@@ -1,2 +1,8 @@
-for s.value = 1:1:10
+function R = test(theta)
+if 251.3/180*pi<=mod(theta,2*pi) && mod(theta,2*pi)<=288.7/180*pi
+    L = cos(18.7/180*pi);
+    R = L / cos(abs(mod(theta,2*pi)-1.5*pi));
+else
+    R = 1;
+end
 end
