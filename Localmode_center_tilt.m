fe2ea1b0c0b27c511a5e406b750e76dba94aa090
Purdy Lab <PURDYLAB@pitt.edu>
2020-01-08 22:02:05 -0500

    debugging... temperature plunging = 0.5 for test now

--- Localmode_center_tilt.m
+++ Localmode_center_tilt.m
@@ -4,11 +4,15 @@ function [localmodefreq, localmodeEffMass]= Localmode_center_tilt(Links,coords)
 eps = 1e-10;
 import com.comsol.model.*
 import com.comsol.model.util.*
-centerline = mphselectbox(model,'geom1', [coords{5}(1),coords{6}(1);coords{5}(2),coords{6}(2);-eps,eps;], 'edge');
-centerlinedata_disp = mpheval(model,'u*u+v*v+w*w','edim',0,'selection',centerline);
-maxpointdata_disp = max(centerlinedata_disp.d1);
+centerline = mphselectbox(model,'geom1', [coords{5}(1)-eps,coords{6}(1)+eps;-eps,eps;-eps,eps;], 'edge');
+centerlinedata_disp = mpheval(model,'u*u+v*v+w*w','edim',1,'selection',centerline);
+
+maxpointdata_disp = zeros(size(centerlinedata_disp.d1,1),1);
+for i=1:size(centerlinedata_disp.d1,1)
+    maxpointdata_disp(i) = max(centerlinedata_disp.d1(i,:));
+end
 temp = mphint2(model,'solid.rho*(u*u+v*v+w*w)',3);
-effectivemass = temp./(maxpointdata_disp.d1)';
+effectivemass = temp./(maxpointdata_disp)';
 [localmodeEffMass, index] = min(effectivemass);
 allfreq = mphglobal(model, 'solid.freq');
 localmodefreq = allfreq(index);
